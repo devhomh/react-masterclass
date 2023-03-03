1ca3381346f48bbe334c60f29c8dba6e11f2b2dd
HosicoOnMyHead <jinwoo7654@naver.com>
2023-03-03 19:52:33 +0900

    file management

---
+++ CryptoCoin/src/routes/Chart.tsx
@@ -0,0 +1,74 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexCharts from "react-apexcharts";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "../atom";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const isDark = useRecoilValue(isDarkAtom);
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexCharts
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: isDark ? "dark" : "light",
+            },
+            chart: {
+              height: 500,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            xaxis: {
+              type: "datetime",
+              categories: data?.map((price) => price.time_close * 1000),
+            },
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
+export default Chart;
---
+++ CryptoCoin/src/routes/Coins.tsx
@@ -0,0 +1,121 @@
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoins } from "../api";
+import { Helmet } from "react-helmet";
+import { useSetRecoilState } from "recoil";
+import { isDarkAtom } from "../atom";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinList = styled.ul`
+  padding: 0px 20px;
+`;
+
+const Coin = styled.li`
+  background-color: ${(props) => props.theme.cardBgColor};
+  color: ${(props) => props.theme.textColor};
+  padding: 20px;
+  border-radius: 15px;
+  margin-bottom: 10px;
+  border: 1px solid white;
+  a {
+    display: flex;
+    align-items: center;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const setterFn = useSetRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setterFn((prev) => !prev);
+  // -----------------react-query 사용-------------------- //
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
+
+  // -----------------react-query 사용하지 않았을때-------------------- //
+  /* const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const json = await (
+        await fetch("https://api.coinpaprika.com/v1/coins")
+      ).json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []); */
+
+  return (
+    <Container>
+      <Helmet>
+        <title>Coin</title>
+      </Helmet>
+      <Header>
+        <Title>Coin</Title>
+        <button onClick={toggleDarkAtom}>Toggle</button>
+      </Header>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinList>
+          {data?.slice(0, 100).map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{ pathname: `/${coin.id}`, state: { name: coin.name } }}
+              >
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id.toLowerCase()}.png`}
+                  alt=""
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
+    </Container>
+  );
+}
+
+export default Coins;
