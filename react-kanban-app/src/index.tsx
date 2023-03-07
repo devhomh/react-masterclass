c246b4f6d6dde1489a0c907c378e9f354c7199c2
HosicoOnMyHead <jinwoo7654@naver.com>
2023-03-07 12:04:50 +0900

    Practice selector(get/set)

---
+++ react-kanban-app/src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme } from "./theme";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </ThemeProvider>
+  </React.StrictMode>
+);
