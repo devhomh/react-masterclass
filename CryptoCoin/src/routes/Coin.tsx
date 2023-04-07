4a597ce1d2ca9612605e296e008cac5432650808
HosicoOnMyHead <jinwoo7654@naver.com>
2023-04-08 00:43:04 +0900

    Add night-mode & back button

--- CryptoCoin/src/routes/Coin.tsx
+++ CryptoCoin/src/routes/Coin.tsx
@@ -12,6 +12,9 @@ import Chart from "./Chart";
 import { useQuery } from "react-query";
 import { fetchCoinInfo, fetchCoinTickers } from "../api";
 import { Helmet } from "react-helmet";
+import { useRecoilState } from "recoil";
+import { isDarkAtom } from "../atom";
+import { motion } from "framer-motion";
 
 const Overview = styled.div`
   display: flex;
@@ -72,6 +75,7 @@ const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 `;
 
 const Title = styled.h1`
@@ -109,6 +113,13 @@ interface InfoData {
   last_data_at: string;
 }
 
+const Svg = styled.svg`
+  width: 35px;
+  height: 35px;
+  position: absolute;
+  cursor: pointer;
+`;
+
 interface IPriceData {
   id: string;
   name: string;
@@ -144,6 +155,8 @@ interface IPriceData {
 }
 
 function Coin() {
+  const [isDark, setIsDark] = useRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setIsDark((prev) => !prev);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
   const priceMatch = useRouteMatch("/:coinId/price");
@@ -189,9 +202,36 @@ function Coin() {
         </title>
       </Helmet>
       <Header>
+        <Link to={{ pathname: "/" }}>
+          <Svg
+            style={{ left: 20, top: 25 }}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              fill={isDark ? "white" : "Black"}
+              d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+            />
+          </Svg>
+        </Link>
         <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
         </Title>
+        <Svg
+          style={{ right: 20, top: 25 }}
+          xmlns="http://www.w3.org/2000/Svg"
+          viewBox={isDark ? "0 0 512 512" : "0 0 384 512"}
+          onClick={toggleDarkAtom}
+        >
+          <motion.path
+            fill={isDark ? "white" : "Black"}
+            d={
+              isDark
+                ? "M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"
+                : "M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"
+            }
+          />
+        </Svg>
       </Header>
       {loading ? (
         <Loader>Loading...</Loader>
--- CryptoCoin/src/routes/Coins.tsx
+++ CryptoCoin/src/routes/Coins.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { fetchCoins } from "../api";
 import { Helmet } from "react-helmet";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { isDarkAtom } from "../atom";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -17,6 +19,7 @@ const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 `;
 
 const CoinList = styled.ul`
@@ -40,6 +43,13 @@ const Coin = styled.li`
       color: ${(props) => props.theme.accentColor};
     }
   }
+`;
+
+const Svg = styled.svg`
+  width: 35px;
+  height: 35px;
+  position: absolute;
+  cursor: pointer;
 `;
 
 const Title = styled.h1`
@@ -68,8 +78,8 @@ interface ICoin {
 }
 
 function Coins() {
-  const setterFn = useSetRecoilState(isDarkAtom);
-  const toggleDarkAtom = () => setterFn((prev) => !prev);
+  const [isDark, setIsDark] = useRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setIsDark((prev) => !prev);
   // -----------------react-query 사용-------------------- //
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
 
@@ -93,7 +103,21 @@ function Coins() {
       </Helmet>
       <Header>
         <Title>Coin</Title>
-        <button onClick={toggleDarkAtom}>Toggle</button>
+        <Svg
+          style={{ right: 20 }}
+          xmlns="http://www.w3.org/2000/Svg"
+          viewBox={isDark ? "0 0 512 512" : "0 0 384 512"}
+          onClick={toggleDarkAtom}
+        >
+          <motion.path
+            fill={isDark ? "white" : "Black"}
+            d={
+              isDark
+                ? "M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"
+                : "M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"
+            }
+          />
+        </Svg>
       </Header>
       {isLoading ? (
         <Loader>Loading...</Loader>
